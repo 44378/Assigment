919f470e241681293b970cbdee11ed8896a42085
44378 <44378@PC004144.coll.lr.local>
2014-09-12 14:34:21 +0100

    Extra tasks

---
+++ Workings/Revision Excercises/2.2.py
@@ -0,0 +1,15 @@
+#Jack Scaife
+#12/09/2014
+#Revision task 1- Multiplication & Division
+
+
+
+first = int(input("Please input your first number : "))
+second = int(input("Please input your second number : "))
+third = int(input("Please input your third number : "))
+
+total=first+second
+end=total/third
+
+print("First number, multiplied by the second number, divided by the third number = {0}".format(end))
+
---
+++ Workings/Revision Excercises/Task 1.1.py
@@ -0,0 +1,14 @@
+#Jack Scaife
+#12/09/2014
+#Revision task 1- Addition
+
+
+print("To work out the total of four numbers...")
+first = int(input("Please input your first number : "))
+second = int(input("Please input your second number : "))
+third = int(input("Please input your third number : "))
+fourth = int(input("Please input your fourth number : "))
+                 
+total=(first+second+third+fourth)
+
+print("Your total is {0}".format(total))
