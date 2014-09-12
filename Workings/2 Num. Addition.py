fdfd7b9173c37abc812e0b538cb4551dd0295ece
44378 <44378@PC004144.coll.lr.local>
2014-09-12 14:16:34 +0100

    EOL Error corrected

---
+++ Workings/2 Num. Addition.py
@@ -0,0 +1,8 @@
+#Jack Scaife
+#12/09/2014
+#Scores task
+
+first_score = int(input("Please give your first number : "))
+second_score = int(input("Please give your second number : "))
+
+print("Your overall score is : {0}".format(first_score + second_score))
---
+++ Workings/Circle Equations - Fixed.py
@@ -0,0 +1,16 @@
+#Jack Scaife
+#variable improvement exercise - edited
+#12/09/2014
+
+import math
+
+r = int(input("please enter the radius of the circle: "))
+
+c = 2* math.pi * r2
+c = round(c,2)
+
+a = math.pi * r**2
+a = round(a,2)
+
+print("The circumference of this circle is {0}.".format(c))
+print("The area of this circle is {0}.".format(a))
