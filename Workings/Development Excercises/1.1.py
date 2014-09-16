436c2f1e66ab1bbf097b9662f4c0df7edb75ca9b
44378 <44378@PC004144.coll.lr.local>
2014-09-16 12:59:04 +0100

    Update of work

---
+++ Workings/Development Excercises/1.1.py
@@ -0,0 +1,9 @@
+number1= int(input("Please input your first number"))
+number2= int(input("Please input your second number"))
+
+div = number1//number2
+mod = number1 % number2
+
+
+print("The division results in {0} remainder {1}".format(div,mod))
+
---
+++ Workings/Development Excercises/2.2.py
@@ -0,0 +1,10 @@
+#Jack Scaife
+#16/09/2014
+#Degrees conversion request
+
+initial_temp = float(input("Please input the temperature in degrees Fahrenheit :"))
+
+centigrade1 = initial_temp - 32
+centigrade2 = centigrade1* 5/9
+
+print("Your temerature in centigrade is {0}".format(centigrade2))
